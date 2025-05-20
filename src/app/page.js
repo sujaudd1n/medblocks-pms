dfbe2c48f3a7bbb9e3800cf924f8a41cb4b9d9e1
Md Sujauddin Sekh <61101752+sujaudd1n@users.noreply.github.com>
2025-05-20 09:15:00 +0530

    configure ui lib

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
+// import PG from "./pg";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1>Patient Management System</h1>
+    <>
+      <h1 className="">Patient Management System</h1>
+    <Button>Register Patient</Button>
+      {/* <PG /> */}
+    </>
   )
 }
